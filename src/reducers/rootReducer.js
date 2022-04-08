5940018534ad4cb0654031787947435d7144cd76
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-08 17:46:00 -0400

    supabase connection with .env working

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,4 @@
 import { combineReducers } from "redux";
 import user from "./userReducer";
-// import other reducers here and put them in the object below
 
 export default combineReducers({ user });
