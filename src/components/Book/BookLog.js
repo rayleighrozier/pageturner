2463ecc5ef8c9d6f5b8e11cb4571a328d5018a27
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-04-12 15:09:07 -0400

    logs making it to database

--- src/components/Book/BookLog.js
+++ src/components/Book/BookLog.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BookRating from "./BookRating";
 import { useSelector, useDispatch } from "react-redux";
 import { ADD_BOOK_LOG, SET_NEW_ENTRY } from "../../action-types";
+import { userUpdateBooks } from "../../actions/supabase";
 import BookEntryForm from "./BookEntryForm";
 
 export default function BookLog() {
@@ -9,33 +10,34 @@ export default function BookLog() {
   let newEntry = useSelector((state) => state.newEntry);
   let allBooks = useSelector((state) => state.user.books.all);
   let currentBookId = useSelector((state) => state.currentBook.id);
-  let entryToSave = useSelector((state) => state.entryToSave);
-  //
+  const id = useSelector((state) => state.user.id);
   const findCurrentBookIndex = (id, shelf) => {
     let index = shelf.findIndex((book) => book.id === id);
     return index;
   };
-  let currentBookIndex = findCurrentBookIndex(currentBookId, allBooks);
-  let currentBookLog = allBooks[currentBookIndex].log;
-  const addToLog = (index, log) => {
-    currentBookLog = [...currentBookLog, { notes: "heeyyyyy" }];
-    allBooks[currentBookIndex].log = currentBookLog;
-    console.log("all books hopefully updated", allBooks);
-    dispatch({
-      type: ADD_BOOK_LOG,
-      payload: {
-        index: index,
-        updatedLog: log,
-      },
-    });
-  };
+  let currentLogEntries =
+    allBooks[findCurrentBookIndex(currentBookId, allBooks)].log;
   const setNewEntry = () => {
     dispatch({ type: SET_NEW_ENTRY, payload: true });
   };
+  useEffect(() => {
+    console.log("updating books in supabase");
+    userUpdateBooks(id, books);
+  }, [allBooks]);
+
   return (
     <div>
       <BookRating />
-      BookLog
+      {currentLogEntries.map((entry) => (
+        <div>
+          <p>Date</p>
+          <p>{entry.date}</p>
+          <p>Pages Read</p>
+          <p>{entry.pagesRead}</p>
+          <p>Notes</p>
+          <p>{entry.notes}</p>
+        </div>
+      ))}
       <button onClick={setNewEntry}>Add Entry</button>
       {newEntry == true ? <BookEntryForm /> : null}
     </div>
