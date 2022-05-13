628afca73d2fedade805c4724997940ddb80dca3
Rayleigh Rozier <rayleighrozier@gmail.com>
2022-05-12 21:26:08 -0400

    shelf selector fix

--- src/components/Book/BookShelfSelector.js
+++ src/components/Book/BookShelfSelector.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
   EDIT_SHELVES,
@@ -12,6 +12,7 @@ export default function BookShelfSelector() {
   const currentBook = useSelector((state) => state.currentBook);
   const books = useSelector((state) => state.user.books);
   const id = useSelector((state) => state.user.id);
+  const [shelfUpdate, setShelfUpdate] = useState(false);
   const closeShelves = () => {
     dispatch({ type: EDIT_SHELVES, payload: false });
   };
@@ -22,12 +23,14 @@ export default function BookShelfSelector() {
       favorites: e.target.form[1].checked,
       tbr: e.target.form[2].checked,
     };
+    console.log("input", input);
     return input;
   };
   const updateShelves = (e) => {
     let selection = captureSelection(e);
     for (const shelf in selection) {
       if (selection[shelf] === true) {
+        console.log("self is true", shelf);
         dispatch({
           type: ADD_USER_BOOK,
           payload: {
@@ -37,6 +40,7 @@ export default function BookShelfSelector() {
         });
       }
       if (selection[shelf] === false) {
+        console.log("self is flase", shelf);
         dispatch({
           type: REMOVE_USER_BOOK,
           payload: {
@@ -46,9 +50,18 @@ export default function BookShelfSelector() {
         });
       }
     }
-    userUpdateBooks(id, books);
-    closeShelves();
+    setShelfUpdate(true);
+    console.log("current state of shelfUpdate", shelfUpdate);
   };
+
+  useEffect(() => {
+    if (shelfUpdate) {
+      console.log("shelf updateeeee", books);
+      userUpdateBooks(id, books);
+      setShelfUpdate(false);
+      closeShelves();
+    }
+  }, [shelfUpdate]);
 
   return (
     <div className="book-shelf-selector white shadow">
